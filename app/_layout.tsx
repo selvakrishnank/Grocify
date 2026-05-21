e2a8b7b45f28b31e27aa75228b80e7935e5f631d
Selvakrishnan k <selvakrishnan435@gmail.com>
2026-05-21 16:38:10 +0530

    feat: implement social authentication and enhance sign-in screen UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,8 @@ import { ClerkProvider } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
 import { Stack } from "expo-router";
 import "../global.css"
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -11,11 +13,13 @@ if (!publishableKey) {
 
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme()
   return <>
   <ClerkProvider
     publishableKey={publishableKey}
     tokenCache={tokenCache}
   >
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}></ThemeProvider>
     <Stack screenOptions={{headerShown:false}} />
   </ClerkProvider>
   </>;
---
+++ app/hooks/useSocialAuth.ts
@@ -0,0 +1,33 @@
+import { useSSO } from "@clerk/expo";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+const useSocialAuth = () => {
+    const [loadingStrategy, setLoadingStrategy] = useState<string | null>(null);
+    const { startSSOFlow } = useSSO();
+
+    const handleSocialAuth = async (strategy: "oauth_google" | "oauth_github" | "oauth_apple") =>{
+        
+        if (loadingStrategy) return;
+        setLoadingStrategy(strategy)
+
+        try{
+            const {createdSessionId , setActive} = await startSSOFlow({strategy});
+
+            if (!createdSessionId || !setActive) {
+                Alert.alert("Sign-in incomplete", "Sign-in did not complete. Please try again");
+                return
+            }
+            await setActive({session: createdSessionId})
+
+        }catch(error){
+            console.log("Error in Social Auth", error)
+            Alert.alert("Error", "Fail to sign in. Please try again.");
+        }finally{
+            setLoadingStrategy(null)
+        }
+    }
+    return { handleSocialAuth, loadingStrategy }
+}
+
+export default useSocialAuth;
